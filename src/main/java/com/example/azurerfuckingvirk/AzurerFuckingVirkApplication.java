35295da52d8adad65a5e7ce6a1e932d37c2b90a0
Jothom2912 <johannes1504@gmail.com>
2023-09-11 10:58:02 +0200

    first commit

---
+++ src/main/java/com/example/azurerfuckingvirk/AzurerFuckingVirkApplication.java
@@ -0,0 +1,15 @@
+package com.example.azurerfuckingvirk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurerFuckingVirkApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AzurerFuckingVirkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/azurerfuckingvirk/AzurerFuckingVirkApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.azurerfuckingvirk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzurerFuckingVirkApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
